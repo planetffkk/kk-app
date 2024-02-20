5767cd95bec2d2e00300c12a0dfb8f6d0ebde2ba
kk <planet.ffkk91@gmail.com>
2024-02-20 20:38:46 +0900

    product-detail追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductdetailComponent } from './productdetail/productdetail.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
-  // { path: 'detail', component: ProductDetailComponent}
+  { path: 'detail', component: ProductdetailComponent}
 ];
 
 @NgModule({
