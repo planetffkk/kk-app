01571bd95c3b367e62863a4d31d44ff2e1e4b333
kk <planet.ffkk91@gmail.com>
2024-03-04 20:46:17 +0900

    エンドポイント作成

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/products', function(req, res){
+    res.json({'sucess': true})
+})
+
 const PORT = process.env.PORT || '3001';
 
 app.listen(PORT, function() {
