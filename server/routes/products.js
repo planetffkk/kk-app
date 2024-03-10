3c73bab30c10d7c55f4b723ed8f3979a29a0b222
kk <planet.ffkk91@gmail.com>
2024-03-10 21:01:22 +0900

    エンドポイント作成

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../model/product')
 
-
 router.get('', async function(req, res){
     foundProducts = await Product.find({})
+        res.json(foundProducts)
+})
+
+
+router.get('/:productId', async function(req, res){
+    const productId = req.params.productId;
+    foundProducts = await Product.findById(productId)
         res.json(foundProducts)
 })
 
