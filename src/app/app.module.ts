9919267e637aefdddf3c688c25c004fa182b78c4
kk <planet.ffkk91@gmail.com>
2024-03-14 21:43:42 +0900

    フロントエンドとバックエンド結合

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -2,6 +2,7 @@ import { Component ,OnInit} from '@angular/core';
 import { ProductService } from '../shared/product.service';
 
 
+
 @Component({
   selector: 'app-product-listings',
   templateUrl: './product-listings.component.html',
@@ -14,7 +15,40 @@ export class ProductListingComponent implements OnInit{
   constructor(private productService: ProductService){}
 
   ngOnInit() {
-    this.products = this.productService.getProducts()
+    // this.products = this.productService.getProducts()
+
+    const productObservable = this.productService.getProducts()
+    productObservable.subscribe(
+      (data) => { 
+        this.products = data
+      },
+      (err) => { console.log('次のエラーが出力されました' + err);  },
+      () => { console.log('コンプリートされました');  } 
+    )
+
+    // const observable = new Observable((subscriber) => {
+    //   subscriber.next(1);
+    //   subscriber.next(2);
+    //   subscriber.complete();
+    //   setTimeout(() => {
+    //     subscriber.next(4);
+    //     subscriber.complete();
+    //   }, 1000);
+    // });
+
+    // console.log('just before subscribe');
+    // observable.subscribe({
+    //   next(x) {
+    //     console.log('got value ' + x);
+    //   },
+    //   error(err) {
+    //     console.error('something wrong occurred: ' + err);
+    //   },
+    //   complete() {
+    //     console.log('done');
+    //   },
+    // });
+    // console.log('just after subscribe');
   }
 
 }
